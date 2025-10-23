9db5c39d157e7dcb4b43ccc468c6ddce5a4dafd9
orlando-paulo99 <orlandopaulo8]7@gmail.com>
2025-10-23 18:57:22 -0300

    ajuste script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const info = document.getElementById("inp")
 const receber= document.getElementById("inserir")
+const total=document.getElementById("total")
 
 receber.addEventListener("keydown" , (e) =>{
 
@@ -8,10 +9,11 @@ receber.addEventListener("keydown" , (e) =>{
         e.preventDefault(); //“Ei navegador, não faça sua ação padrão, eu vou cuidar disso!”  
         
         //Nesse codigo insere o valor o input e é adicionado abaixo dos que já estão
-        info.innerHTML += receber.value +" "+ Date() +"<br><br>";
+        info.innerHTML += "Valor Inserido: " +receber.value + "<br>"+ "Valor Total em Caixa: " + total.value + "<br>" + new Date().toLocaleDateString() +"<br><br>";
         
         //Nesse codigo informa que apos é adicionado o campo do input fica vazio
         receber.value=""
+        total.value=""
     
 
     }
