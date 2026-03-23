687d771116f484ddbf859f3d95f90384ea0615c4
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2026-03-23 13:22:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const saldo=document.getElementById("vTotal")
 
 
 //codigos supabase
-const supabaseUrl = 'https://ofxudymzwrgluaeppcnp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHVkeW16d3JnbHVhZXBwY25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzg4MTMsImV4cCI6MjA3NjkxNDgxM30.D8O_WBcrBrZfbj84cerqjwFGFeUwWXVeOaD9jHPSBR4';
+const supabaseCliente = window.supabase.createClient(
+  "https://ofxudymzwrgluaeppcnp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHVkeW16d3JnbHVhZXBwY25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzg4MTMsImV4cCI6MjA3NjkxNDgxM30.D8O_WBcrBrZfbj84cerqjwFGFeUwWXVeOaD9jHPSBR4"
+);
 
 // forma correta v2
 const { createClient } = window.supabase;
